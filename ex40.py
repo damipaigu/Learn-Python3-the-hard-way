2c9fe1e4a2324e19220167106808029f3f3913db
Mac_damipaigu <ryukenhin@icloud.com>
2018-09-20 19:31:02 +1000

    update

--- ex40.py
+++ ex40.py
@@ -6,11 +6,7 @@ class Song(object):
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
-<<<<<<< HEAD
 # how did the object goes to lyrics???这参数不同名啊 怎么传递过去的？？？
-=======
-# 为什么lyrics自动获得了object的内容？？？
->>>>>>> d2b40c08ef50e57e3ca936a8678cd0342667b916
 happy_baby = Song(["Happy birthday to you",
                    "I don't want to get sued",
                    "So I'll stop right there"])
