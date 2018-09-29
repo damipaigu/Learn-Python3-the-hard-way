6602e3453ae1971383be0a40a9b4e612a7d2df08
Mac_damipaigu <ryukenhin@icloud.com>
2018-09-29 17:10:48 +1000

    update

--- ex40.py
+++ ex40.py
@@ -6,7 +6,8 @@ class Song(object):
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
-# how did the object goes to lyrics???这参数不同名啊 怎么传递过去的？？？
+# how did the object goes to lyrics???这参数不同名啊 怎么传递过去的？
+# 答：__init__函数会自动接收实例化时的参数，可以使多个。
 happy_baby = Song(["Happy birthday to you",
                    "I don't want to get sued",
                    "So I'll stop right there"])
